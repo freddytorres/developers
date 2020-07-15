85e2ff2e5ac68cdf668783a6af35ad25cc474395
freddyrt17 <freddyrt17@hotmail.com>
2020-07-15 18:27:07 -0500

    inicio

---
+++ src/test/java/com/developers/DevelopersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.developers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevelopersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
