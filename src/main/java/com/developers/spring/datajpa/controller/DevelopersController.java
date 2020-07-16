52dcfeb4b941c12a47329837be0126852231202e
freddyrt17 <freddyrt17@hotmail.com>
2020-07-15 20:36:07 -0500

    test5

--- src/main/java/com/developers/spring/datajpa/controller/DevelopersController.java
+++ src/main/java/com/developers/spring/datajpa/controller/DevelopersController.java
@@ -3,10 +3,14 @@ package com.developers.spring.datajpa.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class DevelopersController {	
-	@RequestMapping("/developers")
+	//@RequestMapping("/developers")
+	//@RequestMapping(value = "/{id}", method = RequestMethod.GET)
+	@RequestMapping(value = "/developers", method = RequestMethod.GET)
+	
     @ResponseBody
     String home() {
       return "Hello World1";
