5f5dd1c62fe0dca2128fee0caf094b8ca0918ebd
freddyrt17 <freddyrt17@hotmail.com>
2020-07-15 21:25:46 -0500

    test9

--- src/main/java/com/developers/spring/datajpa/controller/TecnologiaController.java
+++ src/main/java/com/developers/spring/datajpa/controller/TecnologiaController.java
@@ -1,7 +1,8 @@
 package com.developers.spring.datajpa.controller;
 
 import java.util.List;
-
+import javax.validation.Valid;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,17 +14,19 @@ import com.developers.spring.datajpa.repository.TecnologiaRepository;
 
 @Controller
 public class TecnologiaController {
-	
-	
+
 	@Autowired
 	TecnologiaRepository tecnologiaRepository;
-	
-	
-@RequestMapping(value = "/tecnologia", method = RequestMethod.GET)
-	
-    @ResponseBody
-    public List<Tecnologia> getTecnologia() {
+
+	@RequestMapping(value = "/tecnologia", method = RequestMethod.GET)
+	@ResponseBody
+	public List<Tecnologia> getTecnologia() {
 		return tecnologiaRepository.findAll();
 	}
-
+	
+	@RequestMapping(value = "/tecnologia", method = RequestMethod.POST)
+	@ResponseBody
+	public Tecnologia createTecnologia(@Valid @RequestBody Tecnologia tecnologia) {
+		return tecnologiaRepository.save(tecnologia);
+	}
 }
