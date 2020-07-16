6732b37ce9abebfc1e0605a9d96e01def59188da
freddyrt17 <freddyrt17@hotmail.com>
2020-07-15 22:15:48 -0500

    test12

--- src/main/java/com/developers/spring/datajpa/controller/DevelopersController.java
+++ src/main/java/com/developers/spring/datajpa/controller/DevelopersController.java
@@ -4,12 +4,15 @@ import java.util.List;
 
 import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
+import com.developers.spring.datajpa.exception.ResourceNotFoundException;
 import com.developers.spring.datajpa.model.Developers;
 import com.developers.spring.datajpa.repository.DevelopersRepository;
 
@@ -24,10 +27,17 @@ public class DevelopersController {
 		return developersRepository.findAll();
 	}
 
-	@RequestMapping(value = "/developers", method = RequestMethod.POST)
+	@RequestMapping(value = "/developers/{id}", method = RequestMethod.PUT)
 	@ResponseBody
-	public Developers createDevelopers(@Valid @RequestBody Developers developers) {
-		return developersRepository.save(developers);
+	public ResponseEntity<Developers> updateDevelopers(@PathVariable(value = "id") Long developerId,
+			@Valid @RequestBody Developers developersDetails) throws ResourceNotFoundException {
+		Developers developers = developersRepository.findById(developerId)
+				.orElseThrow(() -> new ResourceNotFoundException("Developers no se encuentra : " + developerId));
+		developers.setCedula(developersDetails.getCedula());
+		developers.setNombre(developersDetails.getNombre());
+		developers.setApellido(developersDetails.getApellido());
+		final Developers updatedDevelopers = developersRepository.save(developers);
+		return ResponseEntity.ok(updatedDevelopers);
 	}
 
 }
